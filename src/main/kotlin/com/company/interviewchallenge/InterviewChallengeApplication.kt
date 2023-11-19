987f0f14a335ba9d33e953ab27939469ab04d5cd
veronica ohashi <veronica.ohashi@outlook.com>
2023-11-19 20:18:23 +0000

    initial commit

---
+++ src/main/kotlin/com/company/interviewchallenge/InterviewChallengeApplication.kt
@@ -0,0 +1,11 @@
+package com.company.interviewchallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InterviewChallengeApplication
+
+fun main(args: Array<String>) {
+	runApplication<InterviewChallengeApplication>(*args)
+}
